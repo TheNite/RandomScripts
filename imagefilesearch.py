0de7d9a506dc30138f02649137825ee825660b6a
Paul Cruz <TheNite@users.noreply.github.com>
2020-07-23 18:18:46 -0400

    randoms scripts

---
+++ imagefilesearch.py
@@ -0,0 +1,8 @@
+import os
+
+pic_ext = ['.jgp', '.jpeg', '.heic', '.png', '.heif', '.gif', '.raw']
+for root, folders, files in os.walk("C:\\"):
+    for file in files:
+        if os.path.splitext(file)[1] in pic_ext:
+            with open('pdf_files.txt', 'a') as log_file:
+                log_file.write(f'File: {file:<100s} Location: {os.path.abspath(os.path.join(root, file)):<100s}\n')
