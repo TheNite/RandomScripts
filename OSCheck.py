e814d30f0a9266b96e7575fb61b5e311c52faee8
Paul <paul@Pauls-MBP.noxian.net>
2020-08-16 22:33:44 -0400

    Added a check to make sure its macos

--- OSCheck.py
+++ OSCheck.py
@@ -4,6 +4,8 @@ import sys
 
 home = os.path.expanduser('~')
 
-if platform.system() != 'Windows':
+macOS, Windows, Linux = 'Darwin', 'Windows', 'Linux'
+
+if platform.system() != Windows and platform.system() == macOS:
     os.system("""osascript -e 'tell application (path to frontmost application as text) to display dialog "Please run script on windows" buttons {"OK"} with icon caution'""")
     sys.exit()
