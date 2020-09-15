e61bd785d43645c8eadcceb3b766d5e03f09156d
Paul <paul@Pauls-MBP.noxian.net>
2020-09-15 09:23:16 -0400

    Looker Script

--- looker.py
+++ looker.py
@@ -3,11 +3,14 @@ import looker_sdk
 looker = looker_sdk.init31("./looker.ini")
 dict = {}
 
+allow_admins = []
+
 roles = {
     'Admin': [2],
     'Developer': [4],
     'User': [3],
     'Viewer': [5],
+    'Lite Admin': [6],
 }
 
 def get_all_users():
@@ -18,5 +21,25 @@ def get_all_users():
 for user in get_all_users():
     dict[user.id] = [user.email, user.role_ids]
 
-for id, email in sorted(dict.items()):
-    print(f'ID: {id}, Email: {email}')
+
+def get_key(val: list):
+    for key, value in roles.items():
+        if len(val) > 1:
+            return val
+        elif val == value:
+            return key
+
+
+# for id, [email, role] in sorted(dict.items()):
+#     if email not in allow_admins and not email.endswith('@looker.com'):
+#         for role in looker.user_roles(id):
+#             if role.name == 'Admin':
+#                 #looker.set_user_roles(id, roles['Admin']) # update user role to only have admin
+#
+
+for id, [email, role] in sorted(dict.items()):
+    if not email.endswith('@looker.com') and email not in allow_admins and role == roles['Lite Admin']:
+        print(f'ID: {id}, Email: {email}, Role: {role}')
+        #looker.set_user_roles(id, [])
+
+
